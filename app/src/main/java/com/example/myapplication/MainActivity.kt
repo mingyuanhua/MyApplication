7c44d9742fae95816e5398fe12ba3ec30b7d00c8
mingyuanhua <m.y.hua@hotmail.com>
2023-07-18 18:43:05 -0700

    Add compose and state

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,38 +1,153 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    private val TAG = "lifecycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "We are at onCreate()");
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "We are at onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "We are at onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "We are at onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "We are at onStop()")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "We are at onDestroy()")
+        super.onDestroy()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable // stateless
+private fun HelloContent(name: String, onNameChange: (String) -> Unit) {
+//    // (state, event)
+//    val (name, setName) = remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Hello! ${name}",
+            modifier = Modifier.padding(bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium
+        )
+        OutlinedTextField(
+            value = name,
+            onValueChange = {
+                onNameChange(it)
+            },// setState("dasdas")
+            label = { Text("Name") }
+        )
+        if (name.length > 5) {
+            Text(text = "$name")
+        }
+    }
+}
+
+@Composable //JSX
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier // CSS
+    )
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun ArtistCardColumn() {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()
+        .padding(24.dp)
+        .background(Color.Yellow)
+    ) {
+        Text(text = "hello")
+        Text(text = "World")
+        Text(text = "Kurt")
+    }
 }
+
+@Composable
+fun ArtistCardRow() {
+    Row {
+        Text(text = "hello")
+        Text(text = "World")
+        Text(text = "Kurt")
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewArtistCardColumn() {
+    MyApplicationTheme {
+        ArtistCardColumn()
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewArtistCardRow() {
+    MyApplicationTheme {
+        ArtistCardRow()
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
